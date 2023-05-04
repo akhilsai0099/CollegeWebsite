3227df2732b38ea6a37d611922ec0734d6af9482
akhilsai0099 <akhilsai0099@gmail.com>
2023-05-04 17:53:19 +0530

    added page to upload data to the database

--- AdminControl/admin.py
+++ AdminControl/admin.py
@@ -1,3 +1,55 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from .models import UserData
+from django import forms
+from django.urls import path
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+import csv
 
 # Register your models here.
+
+
+class CsvImportForm(forms.Form):
+    csv_upload = forms.FileField()
+    csv_upload.label = "Upload a CSV file"
+
+
+class UserDataControl(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [path('upload-csv/', self.upload_csv)]
+        return my_urls+urls
+
+    def upload_csv(self, request):
+
+        if request.method == 'POST':
+            csv_file = request.FILES['csv_upload']
+
+            if not csv_file.name.endswith('.csv'):
+                messages.warning(request, "Please upload a csv file")
+                return HttpResponseRedirect(request.path_info)
+
+            decoded_file = csv_file.read().decode('utf-8').splitlines()
+
+            reader = csv.reader(decoded_file)
+
+            for row in reader:
+                print(row)
+
+                userdata = UserData.objects.update_or_create(rollno=row[0],
+                                                             name=row[1],
+                                                             branch=row[2],
+                                                             email=row[3],
+                                                             phone=row[4],
+                                                             scgpa=row[7],
+                                                             total_credits=row[5],
+                                                             total_grade=row[6])
+                # userdata.save()
+            print("data uploaded")
+
+        form = CsvImportForm()
+        body = {"form": form}
+        return render(request, 'admin/upload-csv.html', body)
+
+
+admin.site.register(UserData, UserDataControl)
--- AdminControl/models.py
+++ AdminControl/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class UserData(models.Model):
+    rollno = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=30)
+    branch = models.CharField(max_length=5)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10)
+    total_credits = models.DecimalField(max_digits=5,decimal_places=2)
+    total_grade = models.CharField(max_length=1)
+    scgpa = models.DecimalField(max_digits=3,decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class  Meta:   #new
+        verbose_name_plural  =  "Semester Data"    
