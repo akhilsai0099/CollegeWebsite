76e8155733f29b6206b838f36c7337393a585a0d
akhilsai0099 <akhilsai0099@gmail.com>
2023-07-04 11:50:09 +0530

    application logic changed

--- course_application/views.py
+++ course_application/views.py
@@ -9,112 +9,146 @@ from AdminControl.models import UserData, HonorsModel, MinorsModel
 
 def logout(request):
     auth.logout(request)
-    return redirect('/')
+    return redirect("/")
 
 
 def loginView(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         if username == "" and password == "":
             messages.error(request, "Username and password cannot be empty")
         else:
-
-            user = auth.authenticate(
-                request, username=username, password=password)
+            user = auth.authenticate(request, username=username, password=password)
 
             if user is not None:
                 auth.login(request, user=user)
-                return HttpResponseRedirect('/')
+                return HttpResponseRedirect("/")
             else:
-                messages.error(request, 'Invalid username or password')
+                messages.error(request, "Invalid username or password")
                 return HttpResponseRedirect(request.path_info)
 
     # Render the login form
-    return render(request, 'login.html')
+    return render(request, "login.html")
 
 
 @login_required
 def compare(request):
-
     user = request.user
     if user:
-        student = UserData.objects.get(rollno=user.username)
-        studentname = student.name
-        studentbranch = student.branch
-        studentcgpa = student.scgpa
-        studentroll = student.rollno
-        studentYear = 23 - int(student.batchCode)
-        return render(request, 'mainpage.html', {'name': studentname, 'branch': studentbranch, 'rollno': studentroll, 'cgpa': studentcgpa, 'year': studentYear})
+        try:
+            student = UserData.objects.filter(rollno=user.username)[0]
+            studentname = student.name
+            studentbranch = student.branch
+            studentcgpa = student.scgpa
+            studentroll = student.rollno
+            studentYear = 23 - int(student.batchCode)
+            return render(
+                request,
+                "mainpage.html",
+                {
+                    "name": studentname,
+                    "branch": studentbranch,
+                    "rollno": studentroll,
+                    "cgpa": studentcgpa,
+                    "year": studentYear,
+                },
+            )
+        except Exception as e:
+            print(e)
 
 
+@login_required
 def onsubmit(request):
-    if request.method == 'POST':
-        cgpa = float(request.POST.get("cgpa", '0.00'))
+    if request.method == "POST":
+        cgpa = float(request.POST.get("cgpa", "0.00"))
         appliedForHonors = request.POST.get("AFH")
         appliedForMinors = request.POST.get("AFM")
 
         student = request.user
         studentdata = UserData.objects.get(rollno=student.username)
-        if cgpa >= 8.0 and appliedForHonors:
-            try:
 
-                data, created = HonorsModel.objects.update_or_create(
-                    rollno=studentdata.rollno,
-                    defaults={
-                        'dept': studentdata.branch,
-                        'scgpa': studentdata.scgpa,
-                    }
-                )
-                if created:
-                    message = f"{student.first_name} applied successfully!"
-                    messages.success(request, message)
-                    print(f"Changes submitted")
-                else:
-                    print(f"Applied for the course {cgpa}")
-                return redirect('/')
-            except Exception as e:
-                print(e)
-                message = "unable to apply"
-                messages.error(request, message)
-                return render(request, 'mainpage.html', {'alerting': message})
+        if appliedForHonors and appliedForMinors:
+            applyHonors(request, studentdata, cgpa)
+            applyMinors(request, studentdata, cgpa)
+        elif appliedForHonors and not appliedForMinors:
+            applyHonors(request, studentdata, cgpa)
+            MinorsModel.objects.filter(rollno=student.username).delete()
+        elif appliedForMinors and not appliedForHonors:
+            applyMinors(request, studentdata, cgpa)
+            HonorsModel.objects.filter(rollno=student.username).delete()
 
-        if cgpa >= 7.5 and appliedForMinors:
-            try:
-                choice1 = request.POST['choice1']
-                choice2 = request.POST['choice2']
+        return redirect("/")
 
-                data, created = MinorsModel.objects.update_or_create(
-                    rollno=studentdata.rollno,
-                    defaults={
-                        'scgpa': studentdata.scgpa,
-                    }
-                )
 
-                if choice2 != "none":
-                    # Only include choice2 in the database update if it is not None
-                    data.courseChoice2 = choice2
-                data.courseChoice1 = choice1
-                data.save()
+def applyHonors(request, studentdata, cgpa):
+    if cgpa >= 8.0:
+        try:
+            data, created = HonorsModel.objects.update_or_create(
+                rollno=studentdata.rollno,
+                defaults={
+                    "dept": studentdata.branch,
+                    "scgpa": studentdata.scgpa,
+                },
+            )
+            if created:
+                message = f"{student.first_name} applied successfully!"
+                messages.success(request, message)
+                print(f"Changes submitted")
+            else:
+                print(f"Applied for the course")
 
-                if created:
-                    message = f"{student.first_name} applied successfully!"
-                    messages.success(request, message)
-                    print(f"Changes submitted")
-                else:
-                    print(f"Applied for the course {cgpa}")
-                return redirect('/')
-            except Exception as e:
-                print(e)
-                message = "unable to apply"
-                messages.error(request, message)
-                return render(request, 'mainpage.html', {'alerting': message})
+        except Exception as e:
+            print(e)
+            message = "unable to apply"
+            messages.error(request, message)
+            return render(request, "mainpage.html", {"alerting": message})
+
+
+def applyMinors(request, studentdata, cgpa):
+    if cgpa >= 7.5:
+        try:
+            choice1 = request.POST["choice1"]
+            choice2 = request.POST["choice2"]
+
+            data, created = MinorsModel.objects.update_or_create(
+                rollno=studentdata.rollno,
+                defaults={
+                    "scgpa": studentdata.scgpa,
+                },
+            )
+
+            if choice2 != "none":
+                # Only include choice2 in the database update if it is not None
+                data.courseChoice2 = choice2
+            data.courseChoice1 = choice1
+            data.save()
+
+            if created:
+                message = f"{student.first_name} applied successfully!"
+                messages.success(request, message)
+                print(f"Changes submitted")
+            else:
+                print(f"Applied for the course")
+        except Exception as e:
+            print(e)
+            message = "unable to apply"
+            messages.error(request, message)
+            return render(request, "mainpage.html", {"alerting": message})
 
 
 def home(request):
     user = request.user
-    context = {
-        'user': user
-    }
-    return render(request, 'base.html', context)
+    message = None
+    honors_applied = HonorsModel.objects.filter(rollno=user.username)
+    minors_applied = MinorsModel.objects.filter(rollno=user.username)
+    if honors_applied.exists() and minors_applied.exists():
+        message = f"You have applied for honors and minors in {minors_applied[0].courseChoice1}"
+    elif honors_applied.exists():
+        message = "You have applied for honors"
+    elif minors_applied.exists():
+        message = f"You have applied for Minors in {minors_applied[0].courseChoice1}"
+
+    context = {"user": user, "message": message}
+    return render(request, "base.html", context)
